f8bb6c96cc039ecf21a7cbf7446762af0acb86c1
mrarjundarwade <mrarjundarwade1710@gmail.com>
2026-09-15 22:34:18 +0530

    Time: 3 ms (6.47%), Space: 19.4 MB (9.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,15 +4,16 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """ 
         for j in range(n):
-            val = nums2[j]
-            inserted = False
+            val=nums2[j]
+            inserted=False
             for i in range(m):
-                if nums1[i] > val:
-                    nums1[i+1:m+1] = nums1[i:m]
-                    nums1[i] = val
-                    m += 1
-                    inserted = True
+                if nums1[i]>val:
+                    nums1[i+1:m+1]=nums1[i:m]
+                    nums1[i]=val
+                    m+=1
+                    inserted=True
                     break
             if not inserted:
-                nums1[m] = val
-                m += 1
+                nums1[m]=val
+                m+=1
+

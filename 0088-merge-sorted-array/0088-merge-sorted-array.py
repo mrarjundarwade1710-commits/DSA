ea1b737852a9bbf0f8e3e3fde8adffcf7a53b2e3
mrarjundarwade <mrarjundarwade1710@gmail.com>
2026-09-15 22:35:09 +0530

    Time: 0 ms (100%), Space: 19.4 MB (39.5%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -2,18 +2,25 @@ class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-        """ 
-        for j in range(n):
-            val=nums2[j]
-            inserted=False
-            for i in range(m):
-                if nums1[i]>val:
-                    nums1[i+1:m+1]=nums1[i:m]
-                    nums1[i]=val
-                    m+=1
-                    inserted=True
-                    break
-            if not inserted:
-                nums1[m]=val
-                m+=1
+        """
+        c = m   # count of valid elements in nums1
 
+        # loop through nums2 and insert into nums1
+        for j in range(n):
+            val = nums2[j]
+            inserted = False
+
+            # try to insert val into the right place
+            for i in range(c):
+                if nums1[i] > val:
+                    # shift elements to the right
+                    nums1[i+1:c+1] = nums1[i:c]
+                    nums1[i] = val
+                    c += 1
+                    inserted = True
+                    break
+
+            # if not inserted, append at the end
+            if not inserted:
+                nums1[c] = val
+                c += 1

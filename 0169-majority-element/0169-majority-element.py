08cc7308b9833693d42d3395b5f0a50f3536af78
mrarjundarwade <mrarjundarwade1710@gmail.com>
2026-09-16 20:14:50 +0530

    Time: 6 ms (62.81%), Space: 21.8 MB (6.29%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        # co=0
+        # ele=nums[0]
+        # for i in nums:
+        #     n=nums.count(i)
+        #     if co<n:
+        #         co=n
+        #         ele=i
+        # return ele 
+        count = 0
+        candidate = None
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        
+        return candidate

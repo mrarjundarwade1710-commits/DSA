6aef1d2dc8fd77ba1898c3b5093bf0f108d77379
mrarjundarwade <mrarjundarwade1710@gmail.com>
2026-09-16 20:34:21 +0530

    Time: 15 ms (19.34%), Space: 21.6 MB (6.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -8,12 +8,30 @@ class Solution:
         #         co=n
         #         ele=i
         # return ele 
-        count = 0
-        candidate = None
-        
-        for num in nums:
-            if count == 0:
-                candidate = num
-            count += (1 if num == candidate else -1)
-        
-        return candidate
+        # nums = [2, 2, 1, 1, 1, 2, 2]
+
+        # count = 0         
+        # candidate = None        
+
+        # for num in nums:
+        #     if count == 0:
+        #         candidate = num
+        #     count += (1 if num == candidate else -1)
+
+        # return candidate
+        # nums = [2, 2, 1, 1, 1, 2, 2]
+        count=0   
+        candi=0   
+        for i in range(0,len(nums)):
+            if count==0:
+                candi=nums[i]
+            if  nums[i]==candi:
+                count+=1
+            else:
+                count-=1
+        return candi
+
+
+
+            # count=count+(1 if i==candi else -1)
+            
